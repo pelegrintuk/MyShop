c1513501870133c2480a397fd9f60b2ed1c4784b
pelegrintuk <92951550+pelegrintuk@users.noreply.github.com>
2022-08-21 23:46:33 +0200

    Se acaba Shop y producto y se empieza orders

---
+++ MyWeb.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWeb.MVC.Startup))]
+namespace MyWeb.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
